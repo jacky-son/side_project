b6f7181734fcd70ceca4936ab8a020003a912101
syson <syson@biztechi.com>
2024-10-14 18:23:23 +0900

    page1 분리

--- src/components/Top.js
+++ src/components/Top.js
@@ -6,10 +6,13 @@ function Top(){
                     <a href={"/page1"}>기준 관리</a>
                 </li>
                 <li>
+                    <a href={"/page1"}>계약 관리1-1</a>
+                </li>
+                <li>
                     <a href={"/page2"}>계약 관리</a>
                 </li>
                 <li>
-                    
+
                 </li>
                 <li>
                     <a href={"/#"}>Menu 4</a>
